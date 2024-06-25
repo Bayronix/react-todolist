84561b619128afe10fd33cf8b33d3fa8abdec43c
Bayronix <151779642+Bayronix@users.noreply.github.com>
2024-06-25 16:29:25 +0200

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,44 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 const App = () => {
-  const [clicks, setClicks] = useState(0);
+  const [values, setValues] = useState({
+    login: "",
+    password: "",
+  });
 
-  useEffect(() => {
-    console.log(`Clicks changed - ${clicks}`);
-  }, [clicks]);
+  const handleChange = (evt) => {
+    setValues({
+      ...values,
+      [evt.target.name]: evt.target.value,
+    });
+  };
+
+  const handleSumit = (evt) => {
+    evt.preventDefault();
+
+    console.log(values);
+
+    setValues({
+      login: "",
+      password: "",
+    });
+  };
 
   return (
-    <div>
-      <button onClick={() => setClicks(clicks + 1)}>
-        You clicked {clicks} times
-      </button>
-      <button onClick={() => setClicks(0)}>Reset</button>
-    </div>
+    <form onSubmit={handleSumit}>
+      <input
+        type="text"
+        name="login"
+        value={values.login}
+        onChange={handleChange}
+      />
+      <input
+        type="password"
+        name="password"
+        value={values.password}
+        onChange={handleChange}
+      />
+      <button type="submit">Login</button>
+    </form>
   );
 };
 
--- src/components/ToDoList/ToDoList.jsx
+++ src/components/ToDoList/ToDoList.jsx
@@ -1,12 +1,22 @@
-const CustomButton = ({ message, children }) => {
-  return <button onClick={() => alert(message)}>{children}</button>;
-};
+import { useId } from "react";
 
-const App = () => {
+const LangSwitcher = ({ lang, setLang }) => {
+  const selectId = useId();
+
   return (
-    <>
-      <CustomButton message="Playing music!">Play some music</CustomButton>
-      <CustomButton message="Uploading your data!">Upload data</CustomButton>
-    </>
+    <div>
+      <label htmlFor={selectId}>Choose language</label>
+      <select
+        id={selectId}
+        value={lang}
+        onChange={(evt) => setLang(evt.target.value)}
+      >
+        <option value="uk">Ukrainian</option>
+        <option value="en">English</option>
+        <option value="pl">Polish</option>
+      </select>
+    </div>
   );
 };
+
+export default LangSwitcher;
